cf0c64d15a384ebcb89883e5bf2c072928c7750c
AlexisBryant-25 <albry@unc.edu>
2025-09-18 15:46:48 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,8 @@
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
-install.packages("shiny"), dependencies = TRUE)
 install.packages("skimr"), dependencies = TRUE)
+install.packages("shiny"), dependencies = TRUE)
+
 
 IRkernel::installspec(user = FALSE)
