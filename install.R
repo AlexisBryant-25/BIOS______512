c8347ef37b8f8a3689a9a7cb16098bb6b4f1f1cc
AlexisBryant-25 <albry@unc.edu>
2025-09-19 15:37:32 -0400

    Update install.R to add Rtsne

--- install.R
+++ install.R
@@ -3,6 +3,7 @@ install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("skimr")
 install.packages("shiny")
+install.packages("Rtsne")
 
 
 IRkernel::installspec(user = FALSE)
